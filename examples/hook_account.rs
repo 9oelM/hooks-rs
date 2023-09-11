f0595f2c6b830f50d42bd50f36bcb30af930b0e4
9oelM <hj923@hotmail.com>
2023-09-11 14:19:38 +0800

    feat: hook_account

---
+++ examples/hook_account.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+
+use hooks_rs::*;
+
+#[no_mangle]
+pub extern "C" fn cbak(_: u32) -> i64 {
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn hook(_: u32) -> i64 {
+    _g(1, 1);
+
+    let hook_account = match hook_account() {
+        Ok(acc) => acc,
+        Err(err) => {
+            rollback(b"hook_account.rs: hook_account() failed.", err.into());
+        }
+    };
+
+    accept(&hook_account, 0);
+}
