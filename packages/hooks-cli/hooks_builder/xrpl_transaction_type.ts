7650643e26a424aaeab2557ff306c7ecd416e640
9oelM <hj923@hotmail.com>
2025-01-29 18:51:57 +0900

    a

--- packages/hooks-cli/hooks_builder/xrpl_transaction_type.ts
+++ packages/hooks-cli/hooks_builder/xrpl_transaction_type.ts
@@ -1,4 +1,42 @@
-export const XrplTransactionType = Object.freeze(
+export const XrplTransactionType: Readonly<{
+  readonly PAYMENT: 0;
+  readonly ESCROW_CREATE: 1;
+  readonly ESCROW_FINISH: 2;
+  readonly ACCOUNT_SET: 3;
+  readonly ESCROW_CANCEL: 4;
+  readonly REGULAR_KEY_SET: 5;
+  readonly OFFER_CREATE: 7;
+  readonly OFFER_CANCEL: 8;
+  readonly TICKET_CREATE: 10;
+  readonly SIGNER_LIST_SET: 12;
+  readonly PAYCHAN_CREATE: 13;
+  readonly PAYCHAN_FUND: 14;
+  readonly PAYCHAN_CLAIM: 15;
+  readonly CHECK_CREATE: 16;
+  readonly CHECK_CASH: 17;
+  readonly CHECK_CANCEL: 18;
+  readonly DEPOSIT_PREAUTH: 19;
+  readonly TRUST_SET: 20;
+  readonly ACCOUNT_DELETE: 21;
+  readonly SET_HOOK: 22;
+  readonly NFTOKEN_MINT: 25;
+  readonly NFTOKEN_BURN: 26;
+  readonly NFTOKEN_CREATE_OFFER: 27;
+  readonly NFTOKEN_CANCEL_OFFER: 28;
+  readonly NFTOKEN_ACCEPT_OFFER: 29;
+  readonly URITOKEN_MINT: 45;
+  readonly URITOKEN_BURN: 46;
+  readonly URITOKEN_BUY: 47;
+  readonly URITOKEN_CREATE_SELL_OFFER: 48;
+  readonly URITOKEN_CANCEL_SELL_OFFER: 49;
+  readonly IMPORT: 97;
+  readonly CLAIM_REWARD: 98;
+  readonly INVOKE: 99;
+  readonly AMENDMENT: 100;
+  readonly FEE: 101;
+  readonly UNL_MODIFY: 102;
+  readonly EMIT_FAILURE: 103;
+}> = Object.freeze(
   {
     PAYMENT: 0,
     ESCROW_CREATE: 1,
